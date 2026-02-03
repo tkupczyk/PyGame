207dacb3aa456a56e75127e96c112e0e3550b112
tkupczyk <kupczyk.tomasz2001@gmail.com>
2026-02-03 21:43:51 +0100

    Added collision and player hitbox

--- Vampire survivor/code/player.py
+++ Vampire survivor/code/player.py
@@ -6,6 +6,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load(join('Vampire survivor','images','player','down','0.png')).convert_alpha()
         self.rect = self.image.get_frect(center = pos)
+        self.hitbox_rect = self.rect.inflate(-60, -10)
 
         # movement
         self.collision_sprites = collision_sprites
@@ -19,7 +20,24 @@ class Player(pygame.sprite.Sprite):
         self.direction = self.direction.normalize() if self.direction else self.direction
 
     def move(self, dt):
-        self.rect.center += self.direction * self.player_speed * dt
+        self.hitbox_rect.x += self.direction.x * self.player_speed * dt
+        self.collision('horizontal')
+        self.hitbox_rect.y += self.direction.y * self.player_speed * dt
+        self.collision('vertical')
+        self.rect.center = self.hitbox_rect.center
+
+    def collision(self, direction):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.hitbox_rect):
+                if direction == 'horizontal':
+                    if self.direction.x > 0: self.hitbox_rect.right = sprite.rect.left
+                    if self.direction.x < 0: self.hitbox_rect.left = sprite.rect.right
+
+                if direction == 'vertical':
+                    if self.direction.y > 0: self.hitbox_rect.bottom = sprite.rect.top
+                    if self.direction.y < 0: self.hitbox_rect.top = sprite.rect.bottom
+
+
 
     def update(self, dt):
         self.input()
