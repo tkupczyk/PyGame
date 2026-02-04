8333d9051c92e9ee3671a4790d8b80c32299e9a3
tkupczyk <kupczyk.tomasz2001@gmail.com>
2026-02-04 20:58:40 +0100

    Added map and player camera

--- Vampire survivor/code/main.py
+++ Vampire survivor/code/main.py
@@ -1,6 +1,8 @@
 from settings import *
 from player import Player
 from sprites import *
+from pytmx.util_pygame import load_pygame
+from groups import AllSprites
 
 from random import randint
 
@@ -14,15 +16,26 @@ class Game:
         self.running = True
 
         # Groups
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
+        self.setup()
 
         # Sprites
         self.player = Player((400,300), self.all_sprites, self.collision_sprites)
-        for i in range(6):
-            x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
-            width, height = randint(50,150), randint(50,150)
-            CollisionSprite((x,y),(width,height), (self.all_sprites, self.collision_sprites))
+
+
+    def setup(self):
+        map = load_pygame(join('Vampire survivor','data','maps','world.tmx'))
+
+        for obj in map.get_layer_by_name('Collisions'):
+            CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), (self.all_sprites, self.collision_sprites))
+
+        for x, y, image in map.get_layer_by_name('Ground').tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites)
+
+        for obj in map.get_layer_by_name('Objects'):
+            CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+
 
     def run(self):
         while self.running:
@@ -38,7 +51,7 @@ class Game:
 
             # draw
             self.screen_surface.fill('black')
-            self.all_sprites.draw(self.screen_surface)
+            self.all_sprites.draw(self.player.rect.center)
 
             pygame.display.update()
 
--- Vampire survivor/code/sprites.py
+++ Vampire survivor/code/sprites.py
@@ -1,8 +1,13 @@
 from settings import *
 
-class CollisionSprite(pygame.sprite.Sprite):
-    def __init__(self, pos, size, groups):
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, pos, surf, groups):
         super().__init__(groups)
-        self.image = pygame.Surface(size)
-        self.image.fill('blue')
-        self.rect = self.image.get_frect(center = pos)
+        self.image = surf
+        self.rect = self.image.get_frect(topleft = pos)
+
+class CollisionSprite(pygame.sprite.Sprite):
+    def __init__(self, pos, surf, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(topleft = pos)
