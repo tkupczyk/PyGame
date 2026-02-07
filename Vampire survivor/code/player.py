7273e35c46af6132e6f84287b3932f360a33ca8b
tkupczyk <kupczyk.tomasz2001@gmail.com>
2026-02-07 13:45:39 +0100

    added loading animation files

--- Vampire survivor/code/player.py
+++ Vampire survivor/code/player.py
@@ -4,6 +4,7 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
+        self.load_images()
         self.image = pygame.image.load(join('Vampire survivor','images','player','down','0.png')).convert_alpha()
         self.rect = self.image.get_frect(center = pos)
         self.hitbox_rect = self.rect.inflate(-60, -90)
@@ -13,6 +14,20 @@ class Player(pygame.sprite.Sprite):
         self.direction = pygame.Vector2()
         self.player_speed = 500
 
+    def load_images(self):
+        self.frames = {
+            'up': [],
+            'down': [],
+            'left': [],
+            'right': []
+        }
+
+        for state in self.frames.keys():
+            for foldder_path, sub_folder, file_names in walk(join('Vampire survivor','images','player')):
+                if file_names:
+                    for file_name in file_names:
+                        full_path = join(foldder_path, file_name)
+                        surf = pygame.image.load(full_path).convert_alpha
     def input(self):
         keys = pygame.key.get_pressed()
         self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
@@ -36,8 +51,6 @@ class Player(pygame.sprite.Sprite):
                 if direction == 'vertical':
                     if self.direction.y > 0: self.hitbox_rect.bottom = sprite.rect.top
                     if self.direction.y < 0: self.hitbox_rect.top = sprite.rect.bottom
-
-
 
     def update(self, dt):
         self.input()
